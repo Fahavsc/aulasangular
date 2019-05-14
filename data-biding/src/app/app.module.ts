17379d9faad0d1883dc6d8115260e4ea824082cf
Fabricio Alves <fabricioavsc@gmail.com>
2019-05-14 12:18:01 -0300

    Two way data bind com styles e classes do html com bootstrap

--- data-biding/src/app/app.module.ts
+++ data-biding/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { DataBidingComponent } from './data-biding/data-biding.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
